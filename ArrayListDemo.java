8a4a7c40f74019823239104e46e5804df2666dae
nhanconghuynh <39796799+nhanconghuynh@users.noreply.github.com>
2019-02-07 14:27:49 -0500

    Second commit _ Fix

--- ArrayListDemo.java
+++ ArrayListDemo.java
@@ -5,7 +5,7 @@ import java.util.*;
 class ArrayListDemo {
     public static void main(String[] args) {
 // create an array list
-        ArrayList<java.lang.String> al = new ArrayList<>();
+        ArrayList<String> al = new ArrayList<>();
         System.out.println("Initial size of al: " +
                 al.size());
 // add elements to the array list
